c781de18eafea821909a4ab3d5c2b5d579ff66f6
tiancaixing <734368350@qq.com>
2018-09-18 18:34:50 +0800

    test
    this is a test

---
+++ fwfwf.py
@@ -0,0 +1,3 @@
+2018-01-14 14:32:51.312 Level=3 class=CCrackCaptchaClient       Func=Logoff                    Err=0         	Enter Logoff
+2018-01-14 14:32:51.313 Level=2 class=CCrackCaptchaClient       Func=Logoff                    Err=0         	 logoff(softname=Bypass)
+2018-01-14 14:32:51.313 Level=3 class=CCrackCaptchaClient       Func=Logoff                    Err=0         	Exit Logoff(used 0ms)
